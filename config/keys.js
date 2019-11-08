f54b234a1c337fbb2c5ad28db4825b69a2151ed4
ndjerrou <n.djerrou@gmail.com>
2019-11-08 15:06:10 +0100

    separated keys dev/prod

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI:
-    'mongodb+srv://ndjerrou:<cx+4C8Ad,yN@cluster0-p5vm9.mongodb.net/bank-acccount-api?retryWrites=true&w=majority'
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./prod');
+} else {
+  module.exports = require('./dev');
+}
